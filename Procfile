f177b7482833b42f637781ec910dcdbfa3fea03e
DrMaxPower <ni.fe@icloud.com>
2022-06-19 16:21:09 +0000

    init new project

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn nicks.wisgi
---
+++ sthlm/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SthlmConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'sthlm'
