67b00aa0d62d51cc45768a3bfa1b632d6e0f94ae
DrMaxPower <ni.fe@icloud.com>
2022-06-21 08:40:16 +0000

    save before jest test

--- static/js/script.js
+++ static/js/script.js
@@ -11,40 +11,44 @@ document.getElementById("scroll-down").addEventListener('click', () => {
 })
 
 // add color on todays day
+function whatDay () {
 const d = new Date()
-let day = d.getDay()
-if (day == 1) {
-    document.getElementById('one').style="background-color: antiquewhite;";
-} else {
-    document.getElementById('one').style="background-color: white;";
+    let day = d.getDay()
+    if (day == 1) {
+        document.getElementById('one').style="background-color: antiquewhite;";
+    } else {
+        document.getElementById('one').style="background-color: white;";
+    }
+    if (day == 2) {
+        document.getElementById('two').style="background-color: antiquewhite;";
+    } else {
+        document.getElementById('two').style="background-color: white;";
+    }
+    if (day == 3) {
+        document.getElementById('three').style="background-color: antiquewhite;";
+    } else {
+        document.getElementById('three').style="background-color: white;";
+    }
+    if (day == 4) {
+        document.getElementById('four').style="background-color: antiquewhite;";
+    } else {
+        document.getElementById('four').style="background-color: white;";
+    }
+    if (day == 5) {
+        document.getElementById('five').style="background-color: antiquewhite;";
+    } else {
+        document.getElementById('five').style="background-color: white;";
+    }
+    if (day == 6) {
+        document.getElementById('six').style="background-color: antiquewhite;";
+    } else {
+        document.getElementById('six').style="background-color: white;";
+    }
+    if (day == 0) {
+        document.getElementById('zero').style="background-color: antiquewhite;";
+    } else {
+        document.getElementById('zero').style="background-color: white;";
+    }
+
 }
-if (day == 2) {
-    document.getElementById('two').style="background-color: antiquewhite;";
-} else {
-    document.getElementById('two').style="background-color: white;";
-}
-if (day == 3) {
-    document.getElementById('three').style="background-color: antiquewhite;";
-} else {
-    document.getElementById('three').style="background-color: white;";
-}
-if (day == 4) {
-    document.getElementById('four').style="background-color: antiquewhite;";
-} else {
-    document.getElementById('four').style="background-color: white;";
-}
-if (day == 5) {
-    document.getElementById('five').style="background-color: antiquewhite;";
-} else {
-    document.getElementById('five').style="background-color: white;";
-}
-if (day == 6) {
-    document.getElementById('six').style="background-color: antiquewhite;";
-} else {
-    document.getElementById('six').style="background-color: white;";
-}
-if (day == 0) {
-    document.getElementById('zero').style="background-color: antiquewhite;";
-} else {
-    document.getElementById('zero').style="background-color: white;";
-}
+    
